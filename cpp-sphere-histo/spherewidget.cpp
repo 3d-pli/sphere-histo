0edbc7220997965ad1d1f6d06a1760767a469c1c
a.roethenbacher <a.roethenbacher@fz-juelich.de>
2020-03-18 11:58:10 +0100

    Points can be read in from .npy file and displayed correctly.

--- cpp-sphere-histo/spherewidget.cpp
+++ cpp-sphere-histo/spherewidget.cpp
@@ -24,5 +24,71 @@ void SphereWidget::resizeGL(int w, int h) {
 }
 
 void SphereWidget::paintGL() {
+        // REACTION TO MOUSE INTERACTION
+        glRotatef(angle_x, 0, 1, 0);
+        glRotatef(angle_y, 1, 0, 0);
+        if (scaleFactor){
+            glScalef(scaleFactor, scaleFactor, scaleFactor);
+            scaleFactor = 0;
+        }
+        // TEST ***
 
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glBegin(GL_TRIANGLE_FAN);
+            glVertex3d(0, 1, 0);
+            glVertex3d(1, 0, 0);
+            glVertex3d(0, 0, 1);
+
+            glVertex3d(0, 1, 0);
+            glVertex3d(0, 0, 1);
+            glVertex3d(-1, 0, 0);
+
+            glVertex3d(0, 1, 0);
+            glVertex3d(-1, 0, 0);
+            glVertex3d(0, 0, -1);
+
+            glVertex3d(0, 1, 0);
+            glVertex3d(0, 0, -1);
+            glVertex3d(1, 0, 0);
+
+        glEnd();
+
+
+        glBegin(GL_POINTS);
+        // GIVEN POINTS + MIRRORED POINTS
+        glColor3f(0.54, 0.90, 1.15);
+        for(auto point : points){
+            glVertex3d(point.x(), point.y(), point.z());
+        }
+
+        // MIRRORED GIVEN POINTS
+        glColor3f(0, 0.53, 0.89);
+        for(auto point : points){
+            glVertex3d(-point.x(), -point.y(), -point.z());
+        }
+        glEnd();
+}
+
+void SphereWidget::mousePressEvent(QMouseEvent * event){
+    this->last_x = event->x();
+    this->last_y = event->y();
+}
+
+void SphereWidget::mouseMoveEvent(QMouseEvent * event){
+    std::cout << event->x() - this->last_x << ", " << event->y() - this->last_y << std::endl; //***
+    double dx = event->x() - this->last_x;
+    double dy = event->y() - this->last_y;
+
+    this->angle_x = dx;
+    this->angle_y = dy;
+
+    this->last_x = event->x();
+    this->last_y = event->y();
+    this->update();
+}
+
+void SphereWidget::wheelEvent(QWheelEvent * event){
+    std::cout << (event->delta()) << std::endl; //***
+    this->scaleFactor = exp(event->delta() / 960.);
+    this->update();
 }
--- cpp-sphere-histo/spherewidget.h
+++ cpp-sphere-histo/spherewidget.h
@@ -4,10 +4,15 @@
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 #include <QVector3D>
+#include <QMouseEvent>
 
 #include <vector>
 #define _USE_MATH_DEFINES   // for compatibility with older systems
 #include <math.h>
+
+#include <cnpy.h>
+
+#include <iostream>
 
 
 class SphereWidget : public QOpenGLWidget, protected QOpenGLFunctions
@@ -16,7 +21,8 @@ class SphereWidget : public QOpenGLWidget, protected QOpenGLFunctions
 
 public:
     SphereWidget(QWidget *parent = 0)
-        : QOpenGLWidget(parent)
+        : QOpenGLWidget(parent),
+          points(std::vector<QVector3D>())
     {
         last_x = 0;
         last_y = 0;
@@ -26,6 +32,20 @@ public:
         radius = 1;
         hAngle = M_PI / 180 * 72;
         vAngle = atan(1/2);
+        cnpy::NpyArray np_points = cnpy::npy_load("../test_data/1.npy");
+        size_t row_size = np_points.shape[0];
+        size_t column_size = np_points.shape[1];
+        assert(column_size == 3);
+        for(unsigned i = 0; i < row_size; ++i){
+            points.push_back(QVector3D(np_points.data<double>()[i],
+                                       np_points.data<double>()[i+(1*row_size)],
+                                       np_points.data<double>()[i+(2*row_size)]));
+        }
+//    TEST ***
+//        int i = 0;
+//        for(auto point : points){
+//            std::cout << ++i << "(" << point.x() << ", " << point.y() << ", " << point.z() << ")" << std::endl;
+//        }
     }
 
     ~SphereWidget(){}
@@ -35,10 +55,12 @@ protected:
     virtual void initializeGL() override;
     virtual void resizeGL(int w, int h) override;
     virtual void paintGL() override;
+    virtual void mousePressEvent(QMouseEvent * event) override;
+    virtual void mouseMoveEvent(QMouseEvent * event) override;
+    virtual void wheelEvent(QWheelEvent * event) override;
 
 private:
     std::vector<QVector3D> points;
-
     double last_x;
     double last_y;
     double scaleFactor;
