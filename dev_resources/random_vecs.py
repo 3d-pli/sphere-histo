5605b5820f0394ca89ac55eac41f101f7ae00c5a
a.roethenbacher <a.roethenbacher@fz-juelich.de>
2020-04-22 15:54:33 +0200

    .gitignore updated

---
+++ dev_resources/random_vecs.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 15:42:13 2020
+
+@author: tyrion
+"""
+import numpy as np
+# import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
+
+
+def random_vectors(size, phi, sigma_phi, alpha, sigma_alpha):
+    """
+    size: number of vectors
+    phi: in-plane angle
+    sigma_phi: standard deviation in plane
+    alpha: out-of-plane angle
+    sigma_alpha: standard deviation out-of-plane
+    
+    Returns:
+    vectors as ndarray [size, 3]
+    """
+    phi_samples = np.random.normal(phi, sigma_phi, size)
+    alpha_samples = np.random.normal(alpha, sigma_alpha, size)
+    
+    vectors = np.empty((size, 3))
+
+    vectors[:, 0] = np.cos(alpha_samples) * np.cos( phi_samples )
+    vectors[:, 1] = np.sin(alpha_samples) * np.sin( phi_samples )
+    vectors[:, 2] = np.sin(alpha_samples)
+    return vectors
+
+
+vecs = random_vectors(50000, 0.2, 0.2, 0, 0.2)
+np.save('../test_data/test2', vecs)
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1,projection = '3d')
+# ax.scatter(vecs[:, 0], vecs[:, 1], vecs[:, 2])
+
+# plt.show()
